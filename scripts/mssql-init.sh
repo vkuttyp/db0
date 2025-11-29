18425a6722bfa9ddbe84b9b25ffec3a32bd9e2fe
Veeran Puthumkara <vkuttyp@gmail.com>
2025-11-29 21:29:54 +0300

    fixed docker-compose.yml

--- scripts/mssql-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Wait for SQL Server to start
-sleep 30s
-
-# Run the setup script to create the database
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyStrong!Passw0rd -d master -Q "IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'TestDB') CREATE DATABASE TestDB"
-
-echo "TestDB database created successfully"
